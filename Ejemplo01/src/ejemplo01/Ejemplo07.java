fe5067cd101069ce8d9b586aef8c0aa5f95c2248
ericroco <erickrodas559@gmail.com>
2023-11-01 16:09:00 -0500

    cambios en clase 3

--- Ejemplo01/src/ejemplo01/Ejemplo07.java
+++ Ejemplo01/src/ejemplo01/Ejemplo07.java
@@ -10,16 +10,17 @@ package ejemplo01;
  * @author reroes
  */
 public class Ejemplo07 {
+
     public static void main(String[] args) {
         // Ejemplo de uso de printf con decimales
-        
+
         double numerador = 10.0;
         double denominador = 3.0;
         double resultado = numerador / denominador;
-        
+
         System.out.printf("El resultados es: %f\n", resultado);
-        
+
         System.out.printf("El resultados es: %.2f\n", resultado);
-              
+
     }
 }
