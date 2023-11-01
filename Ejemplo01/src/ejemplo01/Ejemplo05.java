7616880acdd06307ea66e0a2c0107e53f8ed2879
ericroco <erickrodas559@gmail.com>
2023-11-01 15:45:44 -0500

    cambios en clase 2

--- Ejemplo01/src/ejemplo01/Ejemplo05.java
+++ Ejemplo01/src/ejemplo01/Ejemplo05.java
@@ -14,9 +14,9 @@ package ejemplo01;
 public class Ejemplo05 {
     public static void main(String[] args) {
         // Secuencia de escape \n y \t
-        String nombreEstudiante = "René Rolando";
-        String apellidoEstudiante = "Elizalde Solano";
-        int nacimiento = 1983;
+        String nombreEstudiante = "Erick ";
+        String apellidoEstudiante = "Rodas Ordoñez";
+        int nacimiento = 2005;
         
         /*System.out.println(nombreEstudiante+"\n"+apellidoEstudiante+ "\n"+ 
                 nacimiento);
@@ -27,6 +27,9 @@ public class Ejemplo05 {
         System.out.println(nombreEstudiante+"\n"+"\n"+apellidoEstudiante+ "\n"+
                 "\n"+ nacimiento); */
         System.out.println(nombreEstudiante+"\t"+apellidoEstudiante);   
-        System.out.println(nombreEstudiante+"\n\t"+apellidoEstudiante);   
+        System.out.println(nombreEstudiante+"\n\t"+apellidoEstudiante);
+        /* el "\n" sirve para dar un espacio
+        el "\t" sirve para dar un salto de linea
+        */
     }
 }
